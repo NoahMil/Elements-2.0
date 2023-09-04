1889aee0fad5f79402913fc821595c254e265c53
NoahMil <120110545+NoahMil@users.noreply.github.com>
2023-09-04 16:39:53 +0200

    3rd commit

---
+++ Assets/Scripts/Salle Air/ScoreArchery.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class ScoreArchery : MonoBehaviour
+{
+    [SerializeField] private Epreuve AirScriptable;
+    [SerializeField] private ListeTarget _listeTarget;
+    public TextMeshProUGUI ScoreText;
+    public int score = 0;
+    
+    private void OnEnable()
+    {
+        MovingTarget.OnCheckArchery += CheckArchery;
+
+    }
+    private void OnDisable()
+    {
+        MovingTarget.OnCheckArchery -= CheckArchery;
+    }
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    public void UpdateScore()
+    {
+        ScoreText.text = "Cibles : " + score + "/3";
+    }
+
+    private void Update()
+    {
+        UpdateScore();
+    }
+
+    private void CheckArchery()
+    {
+        foreach (Target target in _listeTarget.Targets)
+        {
+            if (target.targetCompleted)
+            {
+                AirScriptable.epreuveCompleted = true;
+                Debug.Log("Complete");
+            }
+        }
+
+    }
+}
