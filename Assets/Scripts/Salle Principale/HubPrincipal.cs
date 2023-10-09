b0b0eeea9a292135f19ce652c14c369e4e47ce59
NoahMil <120110545+NoahMil@users.noreply.github.com>
2023-10-09 13:55:47 +0200

    54th commit

--- Assets/Scripts/Salle Principale/HubPrincipal.cs
+++ Assets/Scripts/Salle Principale/HubPrincipal.cs
@@ -7,6 +7,11 @@ public class HubPrincipal : MonoBehaviour
 {
     [SerializeField] private ListeEpreuve _listeEpreuve;
     [SerializeField] private GameObject finalEscaliers;
+    [SerializeField] private GameObject totem;
+    [SerializeField] private GameObject destroyedPortal;
+    [SerializeField] private GameObject portal;
+    [SerializeField] private GameObject VFXPortal;
+
 
     private void Start()
     {
@@ -15,11 +20,18 @@ public class HubPrincipal : MonoBehaviour
         foreach (Epreuve epreuves in _listeEpreuve.Epreuves)
         {
             { 
-                finalEscaliers.SetActive(false); 
+                finalEscaliers.SetActive(false);
+                totem.SetActive(false);
+                destroyedPortal.SetActive(false);
+                portal.SetActive(true);
+                VFXPortal.SetActive(true);
+
+                /*
                 epreuves.totem.SetActive(false);
                 epreuves.destroyedPortal.SetActive(false);
                 epreuves.vfxPortal.SetActive(true);
                 epreuves.portal.SetActive(true);
+                */
             }
         }
     }
@@ -30,11 +42,17 @@ public class HubPrincipal : MonoBehaviour
         {
             if (epreuves.epreuveCompleted)
             { 
-                finalEscaliers.SetActive(true); 
+                finalEscaliers.SetActive(true);
+                totem.SetActive(true);
+                destroyedPortal.SetActive(true);
+                portal.SetActive(true);
+                VFXPortal.SetActive(false);
+                /*
                 epreuves.totem.SetActive(true);
                epreuves.destroyedPortal.SetActive(true);
                epreuves.vfxPortal.SetActive(false);
                 epreuves.portal.SetActive(false);
+                */
             }
         }
     }
