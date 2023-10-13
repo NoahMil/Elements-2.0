af5e8384acc20e9f605c0d7533f6dfada88a2f21
NoahMil <120110545+NoahMil@users.noreply.github.com>
2023-10-13 11:53:59 +0200

    56th commit

--- Assets/Scripts/Salle Air/Archery/ScoreArchery.cs
+++ Assets/Scripts/Salle Air/Archery/ScoreArchery.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Build.Content;
+// using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class ScoreArchery : MonoBehaviour
